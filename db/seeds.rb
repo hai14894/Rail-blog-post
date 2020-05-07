9dbb3ca02b897898470f02abef94749b9fe4d055
hai14894 <hohoanghai14894@gmail.com>
2020-05-06 20:57:02 -0700

    Hai added nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = ["Technology", "Funny", "Politics", "Travel", "Cooking", "Career", "Fashion"]
+categories.each do |name|
+    Category.create(category: name)
+    puts "#{name} created"
+end
